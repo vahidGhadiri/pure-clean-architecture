16347854944ef2c7c009c08eb8464a9c5348e689
vahid ghadiri <whydrf@gmail.com>
2026-06-29 10:15:00 +0330

    feat(application): add generator types

---
+++ src/application/generator.types.ts
@@ -0,0 +1,11 @@
+import type { TemplateDefinition } from '../domain/template.types.js';
+import type { ITemplateTransformer } from '../domain/interfaces.js';
+import type { DependencySet } from '../domain/dependency.types.js';
+import type { ProjectOptions } from '../domain/project.options.js';
+
+export interface GeneratorContext {
+    extraTransformers?: ITemplateTransformer[];
+    template: TemplateDefinition;
+    dependencies: DependencySet;
+    options: ProjectOptions;
+}
