a6646665060b8b7527e35eab58bc64949d7717a1
vahid ghadiri <whydrf@gmail.com>
2026-07-01 12:15:00 +0330

    feat(application): add usecase generator

---
+++ src/application/generators/code/generators/usecase.generator.ts
@@ -0,0 +1,27 @@
+import type { CodeGeneratorResult, CodeGeneratorInput, CodeGenerator } from '../code-generator.interface.js';
+import { toPascalCase } from '../../../../shared/naming.js';
+
+export class UseCaseGenerator implements CodeGenerator {
+    readonly type = 'usecase';
+
+    async generate(input: CodeGeneratorInput): Promise<CodeGeneratorResult> {
+        const { targetDirectory, name } = input;
+        const pascal = toPascalCase(name);
+
+        return {
+            files: [
+                {
+                    content: `export class ${pascal}UseCase {
+  constructor() {}
+
+  async execute() {
+    // TODO: Implement use case logic
+  }
+}
+`,
+                    path: `${targetDirectory}/src/application/use-cases/${name}.usecase.ts`,
+                },
+            ],
+        };
+    }
+}
