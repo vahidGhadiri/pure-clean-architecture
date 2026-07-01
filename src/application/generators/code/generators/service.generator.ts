aa365aeb0f7708f0a2623a2e9a07c81e76f7fa11
vahid ghadiri <whydrf@gmail.com>
2026-07-01 10:00:00 +0330

    feat(application): add service generator

---
+++ src/application/generators/code/generators/service.generator.ts
@@ -0,0 +1,25 @@
+import type { CodeGeneratorResult, CodeGeneratorInput, CodeGenerator } from '../code-generator.interface.js';
+import { toPascalCase } from '../../../../shared/naming.js';
+
+export class ServiceGenerator implements CodeGenerator {
+    readonly type = 'service';
+
+    async generate(input: CodeGeneratorInput): Promise<CodeGeneratorResult> {
+        const { targetDirectory, name } = input;
+        const pascal = toPascalCase(name);
+
+        return {
+            files: [
+                {
+                    content: `export class ${pascal}Service {
+  constructor() {}
+
+  // TODO: Implement service logic
+}
+`,
+                    path: `${targetDirectory}/src/application/services/${name}.service.ts`,
+                },
+            ],
+        };
+    }
+}
