ba856e6375c878488aacd6d685cd8f8d0e7ad715
vahid ghadiri <whydrf@gmail.com>
2026-07-08 09:30:00 +0330

    feat(shared): add base error and handler

---
+++ src/shared/errors/index.ts
@@ -0,0 +1,6 @@
+export { ValidationError } from './validation.error.js';
+export { FileSystemError } from './filesystem.error.js';
+export { TemplateError } from './template.error.js';
+export { InstallError } from './install.error.js';
+export { BaseError } from './base.error.js';
+export { GitError } from './git.error.js';
