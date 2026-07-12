b3d69ca8f07549d9be4ac6381b3314b639d6a1d2
vahid ghadiri <whydrf@gmail.com>
2026-07-12 11:50:00 +0330

    feat(template): add react/base shared_kernel contracts

---
+++ templates/react/base/src/shared_kernel/contracts/index.ts
@@ -0,0 +1,4 @@
+export type { DeleteCookieParams, SetCookieParams, GetCookieParams, CookieOptions, ICookie } from './cookie.contract';
+export type { RemoveStorageItem, SetStorageData, GetStorageData, IStorage } from './storage.contract';
+export type { RequestConfig, HttpOptions, HttpMethod, IHttp } from './http';
+export type { ErrorType } from './errors';
