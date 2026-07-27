ed5b20c20d85e15df1754b99affc370252474c79
vahidGhadiri <whydrf@gmail.com>
2026-07-27 15:30:43 +0330

    update example page template and repository generator

--- src/application/generators/code/generators/repository.generator.ts
+++ src/application/generators/code/generators/repository.generator.ts
@@ -12,11 +12,11 @@ export class RepositoryGenerator implements CodeGenerator {
       files: [
         {
           content: `export interface ${pascal}Repository {
-  findById(id: string): Promise<${pascal} | null>;
+  findById(params: { pathParams: { id: string } }): Promise<${pascal} | null>;
   findAll(): Promise<${pascal}[]>;
-  create(data: Omit<${pascal}, "id">): Promise<${pascal}>;
-  update(id: string, data: Partial<${pascal}>): Promise<${pascal}>;
-  delete(id: string): Promise<void>;
+  create(params: { body: Omit<${pascal}, "id"> }): Promise<${pascal}>;
+  update(params: { pathParams: { id: string }; body: Partial<${pascal}> }): Promise<${pascal}>;
+  delete(params: { pathParams: { id: string } }): Promise<void>;
 }
 `,
           path: `${targetDirectory}/src/domain/repositories/${name}.repository.ts`,
@@ -25,30 +25,30 @@ export class RepositoryGenerator implements CodeGenerator {
           content: `import type { ${pascal}Repository } from "../../domain/repositories/${name}.repository.js";
 
 export class ${pascal}RepositoryImpl implements ${pascal}Repository {
-  async findById(id: string) {
+  public findById: ${pascal}Repository['findById'] = async ({ pathParams }) => {
     // TODO: Implement persistence logic
     throw new Error("Not implemented");
-  }
+  };
 
-  async findAll() {
+  public findAll: ${pascal}Repository['findAll'] = async () => {
     // TODO: Implement persistence logic
     throw new Error("Not implemented");
-  }
+  };
 
-  async create(data: Parameters<${pascal}Repository["create"]>[0]) {
+  public create: ${pascal}Repository['create'] = async ({ body }) => {
     // TODO: Implement persistence logic
     throw new Error("Not implemented");
-  }
+  };
 
-  async update(id: string, data: Parameters<${pascal}Repository["update"]>[1]) {
+  public update: ${pascal}Repository['update'] = async ({ pathParams, body }) => {
     // TODO: Implement persistence logic
     throw new Error("Not implemented");
-  }
+  };
 
-  async delete(id: string) {
+  public delete: ${pascal}Repository['delete'] = async ({ pathParams }) => {
     // TODO: Implement persistence logic
     throw new Error("Not implemented");
-  }
+  };
 }
 `,
           path: `${targetDirectory}/src/infrastructure/repositories/${name}.repository.impl.ts`,
