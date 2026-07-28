3eabd9ddc19557254b2e1e0b5a0458c9a312dfda
vahidGhadiri <whydrf@gmail.com>
2026-07-28 12:34:52 +0330

    chore(templates/kernel): Inject interceptor dependency to http

--- templates/react/base/src/kernel/shared/infrastructure/http/http.dependencies.ts
+++ templates/react/base/src/kernel/shared/infrastructure/http/http.dependencies.ts
@@ -2,18 +2,20 @@ import type { IResponseBuilder } from './builders/response/response.types';
 import type { IHeadersBuilder } from './builders/headers/headers.types';
 import type { IErrorHandler } from './handlers/error/error.types';
 import type { IUrlBuilder } from './builders/url/url.types';
+import type { InterceptorManager } from './interceptors';
 
 export interface HttpDependencies {
-    readonly responseBuilder: IResponseBuilder;
-    readonly headersBuilder: IHeadersBuilder;
-    readonly errorHandler: IErrorHandler;
-    readonly urlBuilder: IUrlBuilder;
+  readonly interceptorManager: InterceptorManager;
+  readonly responseBuilder: IResponseBuilder;
+  readonly headersBuilder: IHeadersBuilder;
+  readonly errorHandler: IErrorHandler;
+  readonly urlBuilder: IUrlBuilder;
 }
 
 export interface HttpConfig {
-    readonly responseType: 'arrayBuffer' | 'json' | 'text' | 'blob';
-    readonly defaultHeaders: Record<string, string>;
-    readonly cache: RequestCache;
-    readonly baseUrl: string;
-    readonly timeout: number;
+  readonly responseType: 'arrayBuffer' | 'json' | 'text' | 'blob';
+  readonly defaultHeaders: Record<string, string>;
+  readonly cache: RequestCache;
+  readonly baseUrl: string;
+  readonly timeout: number;
 }
