7102e50c8f4cdccf3b5c24db90cb42d858fe06aa
vahidGhadiri <whydrf@gmail.com>
2026-07-28 12:36:16 +0330

    chore(templates/kernel): Add interceptor to http factory

--- templates/react/base/src/kernel/shared/contracts/index.ts
+++ templates/react/base/src/kernel/shared/contracts/index.ts
@@ -1,4 +1,5 @@
 export type { DeleteCookieParams, SetCookieParams, GetCookieParams, CookieOptions, ICookie } from './cookie.contract';
 export type { RemoveStorageItem, SetStorageData, GetStorageData, IStorage } from './storage.contract';
+export type { IInterceptor } from './http/interceptor.contract';
 export type { RequestConfig, HttpOptions, HttpMethod, IHttp } from './http';
 export type { ErrorType } from './errors';
--- templates/react/base/src/kernel/shared/infrastructure/http/http.factory.ts
+++ templates/react/base/src/kernel/shared/infrastructure/http/http.factory.ts
@@ -1,22 +1,30 @@
 import type { HttpDependencies, HttpConfig } from './http.dependencies';
+import type { IInterceptor } from '@shared_kernel/contracts';
+import InterceptorManager from './interceptors/interceptor-manager';
 import ResponseBuilder from './builders/response/response.builder';
 import HeadersBuilder from './builders/headers/headers.builder';
 import ErrorHandler from './handlers/error/error.handler';
 import UrlBuilder from './builders/url/url.builder';
 import Http from './http.client';
 
-function createHttpDependencies(): HttpDependencies {
-    return {
-        responseBuilder: new ResponseBuilder(),
-        headersBuilder: new HeadersBuilder(),
-        errorHandler: new ErrorHandler(),
-        urlBuilder: new UrlBuilder(),
-    };
+export interface CreateHttpOptions<TEndpoints extends Record<string, string>> {
+  readonly interceptors?: readonly IInterceptor[];
+  readonly config?: Partial<HttpConfig>;
+  readonly endpoints: TEndpoints;
+}
+
+function createHttpDependencies(interceptors: readonly IInterceptor[]): HttpDependencies {
+  return {
+    responseBuilder: new ResponseBuilder(),
+    headersBuilder: new HeadersBuilder(),
+    errorHandler: new ErrorHandler(),
+    interceptorManager: new InterceptorManager(interceptors),
+    urlBuilder: new UrlBuilder(),
+  };
 }
 
 export function createHttp<TEndpoints extends Record<string, string>>(
-    endpoints: TEndpoints,
-    config?: Partial<HttpConfig>
+  options: CreateHttpOptions<TEndpoints>
 ): Http<TEndpoints> {
-    return new Http(endpoints, createHttpDependencies(), config);
+  return new Http(options.endpoints, createHttpDependencies(options.interceptors ?? []), options.config);
 }
