fb42e86cda3e075ed5629c38f8bdc0a776a5f2c7
vahidGhadiri <whydrf@gmail.com>
2026-07-28 12:34:03 +0330

    chore(templates/kernel): Expose interceptor contracts

--- templates/react/base/src/kernel/shared/contracts/http/index.ts
+++ templates/react/base/src/kernel/shared/contracts/http/index.ts
@@ -1,1 +1,2 @@
 export type { RequestConfig, HttpOptions, HttpMethod, IHttp } from './http.contract';
+export type { IInterceptor, InterceptorRequest, InterceptorResponse } from './interceptor.contract';
